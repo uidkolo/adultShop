e282beef7e27009c1c74ca9e7272ff5ccd89fa3d
huyang <uidkolo@163.com>
2022-08-24 22:57:07 +0800

    11

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     publicPath: './',
     outputDir: 'dist', //打包输出目录
-    assetsDir: './static', //放置生成的静态资源
+    assetsDir: 'public', //放置生成的静态资源
     filenameHashing: true, // 生成的静态资源在它们的文件名中包含了 hash 以便更好的控制缓存
     lintOnSave: false, //设置是否在开发环境下每次保存代码时都启用 eslint验证
     productionSourceMap: false, // 打包时不生成.map文件
